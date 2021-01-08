dceabf1376dd508289edccb2f9b00530a3d9c4c4
Bujar_Dervishaj <bujar.dervishaj@hotmail.com>
2021-01-08 13:52:47 +0100

    solution cleaned

--- api/PlagarismChecker/src/Infrastructure/DependencyInjection.cs
+++ api/PlagarismChecker/src/Infrastructure/DependencyInjection.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PlagarismChecker.Application.Common.Interfaces;
-using PlagarismChecker.Infrastructure.Files;
 using PlagarismChecker.Infrastructure.Persistence;
 using PlagarismChecker.Infrastructure.Services;
 
@@ -12,29 +11,14 @@ namespace PlagarismChecker.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseInMemoryDatabase("PlagarismCheckerDb"));
-            }
-            else
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(
-                        configuration.GetConnectionString("DefaultConnection"),
-                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-            }
             services.AddDbContext<PlagiarismCheckerDbContext>(options =>
                     options.UseSqlServer(
                         configuration.GetConnectionString("PlagiarismCheckerDbConnection")));
             services.AddScoped<IPlagiarismCheckerDbContext>(provider => provider.GetService<PlagiarismCheckerDbContext>());
 
-            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
-
 
             services.AddTransient<IDateTime, DateTimeService>();
             services.AddTransient<ISearchEngineService, SearchEngineService>();
-            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
 
 
             return services;
